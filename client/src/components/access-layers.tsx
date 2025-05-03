421b89b3bd3f179b9adcc457099a11604de4dfba
mahm1oud <49254657+mahm1oud@users.noreply.github.com>
2025-05-03 23:51:00 +0300

    Delete client directory

--- client/src/components/access-layers.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * تم إزالة مكون الوصول السريع
- * لأن الميزة تم دمجها بشكل كامل في النظام
- */
-
-// This component has been removed as requested
-export default function AccessLayers() {
-  return null;
-}
